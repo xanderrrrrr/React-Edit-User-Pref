a3c4a69ce889201c984846aeca07e0072dc12e27
xanderrr gitlab <xander.canedo@gmail.com>
2021-03-24 23:18:17 -0600

    modified comment to detail what its actually doing

--- edit-user-preferences/client/src/utils/API.js
+++ edit-user-preferences/client/src/utils/API.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-// The getUserPreferences method retrieves recipes from the server
+// The getUserPreferences method retrieves userPreferences from the pointed API in apiRoutes
 // It accepts a "query" or term to search the recipe api for
+// which in this case is the entire URL
 export default {
 
   getUserPreferences: function(query) {
     return axios.get("/api/userPreferences", { params: {q : query } });
   }
 };
-
-
-// if i do my handling in here, I don't need
