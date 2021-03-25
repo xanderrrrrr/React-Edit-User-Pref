52d386d9c849d8079b1d812546ab3a2a6e7eb004
xanderrr gitlab <xander.canedo@gmail.com>
2021-03-25 01:41:14 -0600

    cleaned comment and added TODO

--- edit-user-preferences/client/src/utils/API.js
+++ edit-user-preferences/client/src/utils/API.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 // The getUserPreferences method retrieves userPreferences from the pointed API in apiRoutes
-// It accepts a "query" or term to search the recipe api for
-// which in this case is the entire URL
+// It accepts a "query" so that I can parse the queries out
+// in this case the "q" is the entire URL
 export default {
 
   getUserPreferences: function(query) {
     return axios.get("/api/userPreferences", { params: {q : query } });
   }
 };
+
+// TODO: Will need to add the method for delete in here
